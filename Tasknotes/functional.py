fbed966c587e537b85d092ecb2d9bd502f940fba
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-06 22:51:23 +0300

    Add function find and edit

--- Tasknotes/functional.py
+++ Tasknotes/functional.py
@@ -9,7 +9,7 @@ filename = 'notes3.csv'
 def addnote(): 
     if os.path.exists(filename):   
         with open(filename, "r", newline="") as file:
-            df = pd.read_csv(file)
+            df = pd.read_csv(file, sep=';')
             df['Date'] = pd.to_datetime(df['Date'])
             print('Введите данные для заметки: ')
             name = name_data()
@@ -17,13 +17,53 @@ def addnote():
             dattime = dattime_data()
             df.loc[ len(df.index )] = [dattime, name, text]
             print(df)
-            df.to_csv (r'notes3.csv', index= False )
+            df.to_csv (r'notes3.csv', index= False, sep=';')
     else:
         with open(filename, "w", newline="", encoding='utf-8') as file:
             csv_columns = ['Date','Name','Text']
-            writer = csv.DictWriter(file, fieldnames=csv_columns)
+            writer = csv.DictWriter(file, fieldnames=csv_columns, sep=';')
             writer.writeheader()
             print('Создан новый файл')
             
+def findnote():
+    if os.path.exists(filename):   
+        with open(filename, "r", newline="") as file:
+            df = pd.read_csv(file, sep=';')
+            df['Date'] = pd.to_datetime(df['Date'])
+            dat1=dattime_data()
+            print(dat1)
+            filtr=df.loc[(df['Date'] == dat1)]
+            if(filtr.empty):
+                print('Запись не найдена.')
+            else:
+                print(filtr)
+    else:
+        print('Файл не найден.')
 
-addnote()
+def editnote():
+    if os.path.exists(filename):   
+        with open(filename, "r", newline="") as file:
+            df = pd.read_csv(file, sep=';')
+            df['Date'] = pd.to_datetime(df['Date'])
+            dat1=dattime_data()
+            print(dat1)
+            filtr=df.loc[(df['Date'] == dat1)]
+            if(filtr.empty):
+                print('Запись не найдена.')
+            else:
+                print(filtr)
+                ifiltr=filtr. index [ 0 ]
+                dat2=input('По какому столбцу редактировать запись  (Name или Text)? ')
+                if (dat2 == 'Name'):
+                    df.at[ifiltr, 'Name']= name_data()
+                else:
+                    df.at[ifiltr,'Text'] = text_data()
+                df.at[ifiltr, 'Date'] = dattime_data()
+                print(df)
+                df.to_csv (r'notes3.csv', index= False, sep=';')
+    else:
+        print('Файл не найден.')
+
+#addnote()
+#findnote()
+editnote()
