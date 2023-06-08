a25f6250298ad2f9a7d76d667b1c1f9248c611b0
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-08 13:47:40 +0300

    Final changes functional.py

--- Tasknotes/functional.py
+++ Tasknotes/functional.py
@@ -5,7 +5,7 @@ from datetime import datetime
     
 from data_create import name_data, text_data, dattime_data, id_data
 
-filename = 'notes3.csv'
+filename = 'notes.csv'
 def addnote(): 
     if os.path.exists(filename):   
         with open(filename, "r", newline="") as file:
@@ -16,11 +16,14 @@ def addnote():
             id = id_data()
             text = text_data()
             dattime = dattime_data()
-            dat = datetime.strptime(dattime, "%Y-%m-%d %H:%M")
-            df.loc[ len(df.index )] = [id, dat, name, text]
-            strnew=df.iloc [[df.shape [0]-1]]
-            print(strnew,'\nЗапись сохранена\n')
-            df.to_csv (r'notes3.csv', index= False, sep=';')
+            try:
+                dat = datetime.strptime(dattime, "%Y-%m-%d %H:%M")
+                df.loc[ len(df.index )] = [id, dat, name, text]
+                strnew=df.iloc [[df.shape [0]-1]]
+                print(strnew,'\nЗапись сохранена\n')
+                df.to_csv (r'notes.csv', index= False, sep=';')
+            except ValueError:
+                    print('Дата и время не соответствуют формату. Попробуйте еще раз!')
     else:
         with open(filename, "w", newline="", encoding='utf-8') as file:
             csv_columns = ['ID','Date','Name','Text']
@@ -34,12 +37,16 @@ def findnote():
             df = pd.read_csv(file, sep=';')
             df['Date'] = pd.to_datetime(df['Date'])
             dat1=dattime_data()
-            print(dat1)
-            filtr=df.loc[(df['Date'] == dat1)]
-            if(filtr.empty):
-                print('Запись не найдена\n')
-            else:
-                print(filtr)
+            try:
+                dat1 = datetime.strptime(dat1, "%Y-%m-%d %H:%M")
+                print(dat1)
+                filtr=df.loc[(df['Date'] == dat1)]
+                if(filtr.empty):
+                    print('Заметка не найдена. Уточните дату и время.\n')
+                else:
+                    print(filtr)
+            except ValueError:
+                    print('Дата и время не соответствуют формату. Попробуйте еще раз!')
     else:
         print('Файл не найден\n')
 
@@ -49,21 +56,26 @@ def editnote():
             df = pd.read_csv(file, sep=';')
             df['Date'] = pd.to_datetime(df['Date'])
             dat1=dattime_data()
-            print(dat1)
-            filtr=df.loc[(df['Date'] == dat1)]
-            if(filtr.empty):
-                print('Запись не найдена\n')
-            else:
-                print(filtr)
-                ifiltr=filtr.index [ 0 ]
-                dat2=input('По какому столбцу редактировать запись  (Name или Text)? ')
-                if (dat2 == 'Name'):
-                    df.at[ifiltr, 'Name']= name_data()
+            try:
+                dat1 = datetime.strptime(dat1, "%Y-%m-%d %H:%M")
+                print(dat1)
+                filtr=df.loc[(df['Date'] == dat1)]
+                if(filtr.empty):
+                    print('Заметка не найдена. Уточните дату и время.\n')
                 else:
-                    df.at[ifiltr,'Text'] = text_data()
-                df.at[ifiltr, 'Date'] = dattime_data()
-                print(filtr,'\nЗапись отредактирована\n')
-                df.to_csv (r'notes3.csv', index= False, sep=';')
+                    print(filtr)
+                    ifiltr=filtr.index [ 0 ]
+                    dat2=input('По какому столбцу редактировать заметку  (Name или Text)? ')
+                    if (dat2 == 'Name'):
+                        df.at[ifiltr, 'Name']= name_data()
+                    else:
+                        df.at[ifiltr,'Text'] = text_data()
+                    df.at[ifiltr, 'Date'] = dattime_data()
+                    stredit=df.loc[[ifiltr]]
+                    print(stredit,'\nЗаметка отредактирована\n')
+                    df.to_csv (r'notes.csv', index= False, sep=';')
+            except ValueError:
+                print('Дата и время не соответствуют формату. Попробуйте еще раз!')
     else:
         print('Файл не найден\n')
 
@@ -73,14 +85,18 @@ def delnote():
             df = pd.read_csv(file, sep=';')
             df['Date'] = pd.to_datetime(df['Date'])
             dat1=dattime_data()
-            filtr=df.loc[(df['Date'] == dat1)]
-            if(filtr.empty):
-                print('Запись не найдена\n')
-            else:
-                print(filtr,'\nЗапись удалена\n')
-                ifiltr=filtr. index [ 0 ]
-                df = df.drop (index= ifiltr )
-                df.to_csv (r'notes3.csv', index= False, sep=';')
+            try:
+                dat1=datetime.strptime(dat1, "%Y-%m-%d %H:%M" )
+                filtr=df.loc[(df['Date'] == dat1)]
+                if(filtr.empty):
+                    print('Заметка не найдена. Уточните дату и время.\n')
+                else:
+                    print(filtr,'\nЗаметка удалена\n')
+                    ifiltr=filtr. index [ 0 ]
+                    df = df.drop (index= ifiltr )
+                    df.to_csv (r'notes.csv', index= False, sep=';')
+            except ValueError:
+                    print('Дата и время не соответствуют формату. Попробуйте еще раз!')
     else:
         print('Файл не найден\n')
 
@@ -91,6 +107,28 @@ def shownotes():
             df['Date'] = pd.to_datetime(df['Date'])
             df=df.sort_values (by='Date')
             print(df)
-            df.to_csv (r'notes3.csv', index= False, sep=';')
+            df.to_csv (r'notes.csv', index= False, sep=';')
+    else:
+        print('Файл не найден\n')
+
+def shownotesperiod():
+    if os.path.exists(filename):   
+        with open(filename, "r", newline="") as file:
+            df = pd.read_csv(file, sep=';')
+            df['Date'] = pd.to_datetime(df['Date'])
+            dat1=input('Введите дату начала периода в формате yyyy-mm-dd: ')
+            try:
+                dat1=datetime.strptime(dat1, "%Y-%m-%d")
+                dat2=input('Введите дату окончания периода в формате yyyy-mm-dd: ')
+                try:
+                    filtr=df.loc[(df['Date'] > dat1)&(df['Date'] < dat2)]
+                    if(filtr.empty):
+                        print('Заметок не найдено. Уточните даты.\n')
+                    else:
+                        print(filtr)
+                except ValueError:
+                    print('Дата и время не соответствуют формату. Попробуйте еще раз!')
+            except ValueError:
+                    print('Дата и время не соответствуют формату. Попробуйте еще раз!')
     else:
         print('Файл не найден\n')
