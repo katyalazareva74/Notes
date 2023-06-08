d8fa7b8e6a8d9921c38400b5e253e974b9f7a50d
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-08 13:48:10 +0300

    Final changes view.py

--- Tasknotes/view.py
+++ Tasknotes/view.py
@@ -1,27 +1,30 @@
-from functional import addnote, findnote, editnote, delnote, shownotes
+from functional import addnote, findnote, editnote, delnote, shownotes, shownotesperiod
 
 def view():
     print("""Выберете режим работы:
-            1 - создание записи
-            2 - просмотр записей
-            3 - найти запись
-            4 - удаление записи
-            5 - изменение записи
-            6 - выход
+            1 - создание заметки
+            2 - просмотр заметок
+            3 - просмотр периода заметок
+            4 - найти заметку
+            5 - изменение заметки
+            6 - удаление заметки
+            7 - выход
               """)
     mode = int(input('Введите номер режима: '))
-    while mode < 1 or mode > 6:
+    while mode < 1 or mode > 7:
         mode = int(input('Введите корректный номер: '))
     if mode == 1:
         addnote()
     elif mode == 2:
         shownotes()
     elif mode == 3:
-        findnote()
+        shownotesperiod()
     elif mode == 4:
-        delnote()
+        findnote()
     elif mode == 5:
         editnote()
     elif mode == 6:
+        delnote()
+    elif mode == 7:
         exit()
     view()
